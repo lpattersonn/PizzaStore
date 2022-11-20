17ecb407c3ad3423c8c0d35ac5b7145132f79253
Leshan <leshan0627@gmail.com>
2022-11-20 02:43:28 -0500

    initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PizzaStore.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            PizzaViewModel pizza = new PizzaViewModel()
+            {
+                ID = 1,
+                Name = "Vegetarian",
+                Price = 10,
+                ImageURL = "/images/Pizzas/Vegetarian.png"
+            };
+
+            return View(pizza);
+        }
+
+
+    }
+}
+
---
+++ Models/PizzaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PizzaStore.Models
+{
+	public class PizzaViewModel
+	{
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+
+		public int Price { get; set; }
+
+		public string ImageURL { get; set; }
+	}
+}
+
